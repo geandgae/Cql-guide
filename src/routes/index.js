8d5c99ca80324cbe46b17aa49514d6e894692774
inhwanbaek <134063064+inhwanbaek@users.noreply.github.com>
2023-07-22 13:13:05 +0900

    룸 추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -23,12 +23,7 @@ export default createRouter ({
       name: 'app-main',
       component: () => import("@/views/app-main")
     },
-    // management
-    {
-      path: '/management/roominfo',
-      name: 'roominfo',
-      component: () => import("@/views/management/RoomInfo")
-    },
+  
     // setting
     {
       path: '/setting',
@@ -95,25 +90,106 @@ export default createRouter ({
     },
 
     //객실관리
+    { //12.객실관리 - 입실처리
+      path: '/room/room_in',
+      name: 'check_in',
+      component: () => import("@/views/room/room_in")
+    },
+    { //13.객실예약
+      path: '/room/room_reservation',
+      name: 'room_reservation',
+      component: () => import("@/views/room/room_reservation")
+    },
+    { //14.객실이력조회
+      path: '/room/room_inquiry',
+      name: 'room_inquiry',
+      component: () => import("@/views/room/room_inquiry")
+    },
+    { //15.기간별 매출 집계
+      path: '/room/check_sales',
+      name: 'check_sales',
+      component: () => import("@/views/room/check_sales")
+    },
+    { //16.객실내역
+      path: '/room/room_history',
+      name: 'room_history',
+      component: () => import("@/views/room/room_history")
+    },
+    { //17.객실이력
+      path: '/room/room_event',
+      name: 'room_event',
+      component: () => import("@/views/room/room_event")
+    },
+    { //18.객실설정
+      path: '/room/room_setting',
+      name: 'room_setting',
+      component: () => import("@/views/room/room_event")
+    },
+    { //19.시뮬레이터
+      path: '/room/simulator',
+      name: 'simulator',
+      component: () => import("@/views/room/simulator")
+    },
+    { //20.객실판매현황
+      path: '/room/sales_status',
+      name: 'sales_status',
+      component: () => import("@/views/room/sales_status")
+    },
+    { //23.포인트이력조회
+      path: '/room/sales_status',
+      name: 'sales_status',
+      component: () => import("@/views/room/sales_status")
+    },
+    // management
+    {//24.객실정보 관리
+      path: '/management/roominfo',
+      name: 'roominfo',
+      component: () => import("@/views/management/roomInfo")
+    },
+    { //25.객실타입 관리
+      path: '/room/room_type',
+      name: 'room_type',
+      component: () => import("@/views/room/room_type")
+    },
+    { //26.일괄처리
+      path: '/room/room_status',
+      name: 'room_status',
+      component: () => import("@/views/room/room_status")
+    },
+    { //27.포인트관리
+      path: '/room/point_management',
+      name: 'point_management',
+      component: () => import("@/views/room/point_management")
+    },
+    { //28.예약관리
+      path: '/room/room_reservation',
+      name: 'room_reservation',
+      component: () => import("@/views/room/room_reservation")
+    },
+    { //29.예약등록
+      path: '/room/room_registration',
+      name: 'room_registration',
+      component: () => import("@/views/room/room_registration")
+    },
     { //30.업소 게시판-알림목록
       path: '/room/board_alarm',
       name: 'board_alarm',
-      component: () => import("@/views//room/board_alarm")
+      component: () => import("@/views/room/board_alarm")
     },
     { //31.업소 게시판-알림등록
       path: '/room/board_alarm_write',
       name: 'board_alarm_write',
-      component: () => import("@/views//room/board_alarm_write")
+      component: () => import("@/views/room/board_alarm_write")
     },
     { //32.A/S 건의사항-건의목록
       path: '/room/board_as',
       name: 'board_as',
-      component: () => import("@/views//room/board_as")
+      component: () => import("@/views/room/board_as")
     },
     { //33.A/S 건의사항-건의등록
       path: '/room/board_as_write',
       name: 'board_as_write',
-      component: () => import("@/views//room/board_as_write")
+      component: () => import("@/views/room/board_as_write")
     },
   ]
 })
