c6d0a0969d57bde51c45d2e4f2a12c74bbdf7259
ge <g_e@daum.net>
2023-07-23 18:22:25 +0900

    에러수정

--- src/routes/index.js
+++ src/routes/index.js
@@ -46,47 +46,47 @@ export default createRouter ({
     { //02. 예약현황
       path: '/dashboard/registration_status',
       name: 'registration_status',
-      component: () => import("@/views//dashboard/registration_status")
+      component: () => import("@/views/dashboard/registration_status")
     },
     { //03.ISGA(무인판매기) 
       path: '/dashboard/ISGA',
       name: 'ISGA',
-      component: () => import("@/views//dashboard/ISGA")
+      component: () => import("@/views/dashboard/ISGA")
     },
     { //04.숙박뉴스_목록 
       path: '/dashboard/hotel_news',
       name: 'hotel_news',
-      component: () => import("@/views//dashboard/hotel_news")
+      component: () => import("@/views/dashboard/hotel_news")
     },
     { //05.숙박뉴스_보기 
       path: '/dashboard/hotel_news_view',
       name: 'hotel_news_view',
-      component: () => import("@/views//dashboard/hotel_news_view")
+      component: () => import("@/views/dashboard/hotel_news_view")
     },
     { //06.공지사항_목록 
       path: '/dashboard/notice_news',
       name: 'notice_news',
-      component: () => import("@/views//dashboard/notice_news")
+      component: () => import("@/views/dashboard/notice_news")
     },
     { //07.공지사항_보기 
       path: '/dashboard/notice_news_view',
       name: 'notice_news_view',
-      component: () => import("@/views//dashboard/notice_news_view")
+      component: () => import("@/views/dashboard/notice_news_view")
     },
     { //08.구독현황_목록 
       path: '/dashboard/subscribe_status',
       name: 'subscribe_status',
-      component: () => import("@/views//dashboard/subscribe_status")
+      component: () => import("@/views/dashboard/subscribe_status")
     },
     { //09.구독현황_나의구독 채널 
       path: '/dashboard/subscribe_status_channel',
       name: 'subscribe_status_channel',
-      component: () => import("@/views//dashboard/subscribe_status_channel")
+      component: () => import("@/views/dashboard/subscribe_status_channel")
     },
     { //10.구독현황_보기 
       path: '/dashboard/subscribe_status_view',
       name: 'subscribe_status_view',
-      component: () => import("@/views//dashboard/subscribe_status_view")
+      component: () => import("@/views/dashboard/subscribe_status_view")
     },
 
     //객실관리
@@ -144,7 +144,7 @@ export default createRouter ({
     {//24.객실정보 관리
       path: '/management/roominfo',
       name: 'roominfo',
-      component: () => import("@/views/management/roomInfo")
+      component: () => import("@/views/management/RoomInfo")
     },
     { //25.객실타입 관리
       path: '/room/room_type',
